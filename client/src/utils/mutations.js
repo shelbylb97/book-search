f91cf055b9f3f525ce8d31747576fa669c19e357
Shelby <shelbylb97@comcast.net>
2021-11-07 14:25:33 -0500

    refactoring queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,7 +25,7 @@ export const ADD_USER = gql`
     }
   }
 `;
-//need to be altered
+
 export const SAVE_BOOK = gql`
   mutation saveBook($input: BookInput!) {
     saveBook(input: $input) {
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,15 +2,19 @@
 import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
-  query user($username: String!) {
-    user(username: $username) {
+  query me {
+    me {
       _id
       username
       email
-      thoughts {
-        _id
-        thoughtText
-        createdAt
+      password
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
       }
     }
   }
