3d130d241843bee79d8c3e96f702018dc4ab919e
Shelby <shelbylb97@comcast.net>
2021-11-07 20:28:43 -0500

    refactoring SavedBooks

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -37,31 +37,6 @@ const SavedBooks = () => {
   }
 };
 
-
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const handleDeleteBook = async (bookId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
-    try {
-      const response = await deleteBook(bookId, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
-      removeBookId(bookId);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
   // if data isn't here yet, say so
   if (!userDataLength) {
     return <h2>LOADING...</h2>;
